225d61f9b9ff81858dfbb9fc1afdab8954b8d6b4
[muhammad naeem] <[naeemwarsi20@gmail/com]>
2024-03-31 01:18:29 +0500

    work complete

---
+++ Q21.js
@@ -0,0 +1,5 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = { name: "Amjad", friendName: "Bilal", Age: 28 };
+console.log(personName);
---
+++ Q21.ts
@@ -0,0 +1,5 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let personName ={name: "Amjad",friendName: "Bilal",Age:28}
+
+console.log(personName)
